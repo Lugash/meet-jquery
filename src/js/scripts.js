86610222becdffd914825ec503b0a1386b3fe3bd
Lugash <lucasgava1993@gmail.com>
2017-02-13 10:53:40 +0100

    Exercise part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,20 @@
 // meet jQuery
 // by Lugash
 
-// console.log( "Hello, dude!" );
+const fHandleTabClick = function ( oEvent ) {
+    oEvent.preventDefault();
+    const $target = oEvent.currentTarget;
+
+    if ( $target.parentNode.classList.contains( "active" ) ) {
+        // console.log( "Tab is active!" );
+        return;
+    }
+
+    document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
+    $target.parentNode.classList.add ( "active" );
+    document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
+    document.getElementById( $target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+};
 
 // page loads
 window.addEventListener( "load", function() {
@@ -17,6 +30,9 @@ window.addEventListener( "load", function() {
     } );
 
     // 2. select w/ a rel=external
+    Array.from( document.querySelectorAll( "ul.nav.nav-tabs a" ) ).forEach( function( $elt ) {
+        $elt.addEventListener( "click", fHandleTabClick );
+    } );
 
     // 3. select w/ a rel=external
 
