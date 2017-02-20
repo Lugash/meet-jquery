cbcd503961b6fbbec1a42df11549659cb22bd7bd
Lugash <lucasgava1993@gmail.com>
2017-02-20 09:21:26 +0100

    Add trombino jquery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,7 @@
 
 // variables
 let $tabs;
+let $trombinoFigures;
 
 const fChangeTab = function(oEvent ) {
     oEvent.preventDefault();
@@ -12,6 +13,18 @@ const fChangeTab = function(oEvent ) {
     $( this ).parent().addClass( "active" );
     $( ".tab-content .tab-pane.active" ).removeClass( "atcive" );
     $( `#${ $( this ).data( "tab-target" ) }` ).addClass( "active" );
+};
+
+const fHandleTrombino = function () {
+    // .fadeOut() // .hide() .slideUp()
+    $trombinoFigures.filter( ":visible" ).fadeOut( function () {
+        let $next = $( this ).next();
+        if ( $next.length === 0 ) {
+            $next = $trombinoFigures.first();
+        }
+        $next.fadeIn( 100 ); // .show() .slideDown()
+    } );
+
 };
 
 // call when Dom is ready
@@ -24,5 +37,10 @@ $( function() {
     // $tabs = $( "ul.nav.nav-tabs a" );
     // $tabs.on( "click", fChangeTab );
     ( $tabs = $( "ul.nav.nav-tabs a" ) ).on( "click", fChangeTab );
+
+    // 3. trombinoscope
+    $trombinoFigures = $( "#trombino figure" );
+    $trombinoFigures.hide().first().show();
+    setInterval( fHandleTrombino, 1000 );
 
 } );
